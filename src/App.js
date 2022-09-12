b9406f8655cf203789e4a0787d0a61ece150d8e6
harksu <harksulim@naver.com>
2022-09-13 04:27:02 +0900

    이미지 넣는중

--- src/App.js
+++ src/App.js
@@ -26,17 +26,45 @@ function App() {
     <ScrollContainer>
       <ScrollPage>
         <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200))}>
-          <span style={{ fontSize: "30px" }}>마뚜 😍😍😍 지롱</span>
+          <span>
+            <img
+              src={require("./start.jpg")}
+              style={{ width: "100vw", height: "100vh", opacity: 0.7 }}
+            />
+          </span>
         </Animator>
       </ScrollPage>
       <ScrollPage>
         <Animator animation={ZoomInScrollOut}>
-          <span style={{ fontSize: "40px" }}>5주년을 너무 축하해</span>
+          <span style={{ fontSize: "20px" }}>17.09.16 ~ ing</span>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage>
+        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200))}>
+          <span>
+            <img
+              src={require("./start2.png")}
+              style={{ width: "100vw", height: "100vh", opacity: 1 }}
+            />
+          </span>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage>
+        <Animator animation={ZoomInScrollOut}>
+          <span style={{ fontSize: "20px" }}>
+            😍 우리의 5주년을 너무 축하해 😍 <br />
+            ㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇ어쩌구 저쩌구 저꺼 주구저ㅏㅉ구ㅜ{" "}
+            <br />
+            마지막엔 우리 사진 다 모아서 바둑판 처럼 ㄱㄱ
+          </span>
         </Animator>
       </ScrollPage>
       <ScrollPage>
         <Animator animation={FadeUp}>
-          <span style={{ fontSize: "40px" }}>여기서 어쩌구 저쩌구</span>
+          <span style={{ fontSize: "40px", width: "500px", height: "100px" }}>
+            <img src="./logo512.png" />
+            <img src="./start.jpg" />
+          </span>
         </Animator>
       </ScrollPage>
       <ScrollPage>
@@ -49,6 +77,7 @@ function App() {
           }}
         >
           <span style={{ fontSize: "40px" }}>
+            {/* 여기에선 년도 별로 우리 이벤트들 언급하면서 모아도 좋을듯  */}
             <Animator animation={MoveIn(-1000, 0)}>Hello Guys 👋🏻</Animator>
             <Animator animation={MoveIn(1000, 0)}>Nice to meet you 🙋🏻‍♀️</Animator>
             - I'm Dante Chun -
